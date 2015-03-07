a14db49bd30133de2a4b838f0c684781174fcb5f
Bryan Clark <clarkbw@gmail.com>
2015-03-06 22:33:46 -0500

    update tests to modern sdk
    still one test failing and not sure why

--- tests/test-main.js
+++ tests/test-main.js
@@ -2,42 +2,48 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true,
-  strict:true, undef:true, curly:true, browser:true, es5:true,
-  indent:2, maxerr:50, devel:true, node:true, boss:true, white:true,
+/*jshint esnext:true, node:true,
   globalstrict:true, nomen:false, newcap:true*/
 
-"use strict";
+'use strict';
 
-var tabs = require("tabs");
+var tabs = require('sdk/tabs');
 
-var TEST_FILE = "test-main.js";
-var HTML_FILE = "static/test-page.html";
+var TEST_FILE = 'test-main.js';
+var HTML_FILE = 'static/test-page.html';
 var HTML_URI = module.uri.replace(TEST_FILE, HTML_FILE);
 
-var main = require("main");
+var main = require('main');
 
 // shim in the resource URL to the include options
 // this needs to be a regex as well so it's evaluated before the page-mod looks
 // for http/ftp style urls
 main.mod.include.add(new RegExp(HTML_URI));
 
-exports.test_open_tab = function (test) {
+exports.testOpenTab = function (assert, done) {
   tabs.on('ready', function onReady(tab) {
-    test.assertEqual(tab.url, HTML_URI, "tab url is not correct");
+    assert.equal(tab.url, HTML_URI, 'tab url is not correct');
     tab.attach({
       // we use a timeout here to ensure that our page-mod could run before we report back results
-      contentScript: 'window.setTimeout(function run() { self.postMessage(document.querySelectorAll("[autocomplete=off]").length); }, 3 * 1000);',
+      contentScript: 'window.setTimeout(function run() { ' +
+                      'var elements = document.querySelectorAll("[autocomplete=off]");' +
+                      'Array.prototype.forEach.call(elements, function(el, i){' +
+                      ' console.log(i, el.id);' +
+                      '});' +
+                      'self.postMessage(document.querySelectorAll("[autocomplete=off]").length);' +
+                      '}, 3 * 1000);',
       onMessage: function (message) {
-        test.assertEqual(message, 1, "should have only found 1 item with the autocomplete=off result");
-        test.done();
+        assert.equal(message, 1, 'should have only found 1 item with the autocomplete=off result');
+        tab.close();
+        done();
       }
     });
   });
   tabs.open(HTML_URI);
-  test.waitUntilDone();
 };
 
-exports.test_pref = function (test) {
-  test.assert(require("sdk/preferences/service").get("signon.overrideAutocomplete"));
+exports.testPref = function (assert) {
+  assert.ok(require('sdk/preferences/service').get('signon.overrideAutocomplete'));
 };
+
+require('test').run(exports);
